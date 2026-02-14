2c5adb449b4b646b81dcfd018f9ba2e4c1c01145
kodurusupriya <supriyanaidu113@gmail.com>
2026-02-13 20:14:33 -0500

    actions

---
+++ common.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+set -e # this will be checking for errors, if errors it will exit
+
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+START_TIME=$(date +%s)
+echo -e "$(date "+%Y-%m-%d %H:%M:%S") |Script started executing at: $$(date)" | tee -a $LOGS_FILE
+
+check_root(){
+    if [ $USERID -ne 0 ]; then
+        echo -e "$R please run this script with root user access $N"  | tee -a $LOGS_FILE
+        exit 1
+    fi
+ }
+mkdir -p $LOGS_FOLDER
+
+#by default shell will not execute,only executed when called
+VALIDATE(){
+
+        if [ $1 -ne 0 ]; then 
+            echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $2 ... $R failure $N" | tee -a $LOGS_FILE
+            exit 1
+        else 
+            echo -e "$2 ... $G success $N"  | tee -a $LOGS_FILE
+        fi     
+
+}
+print_total_time(){
+    END_TIME=$(date+%s)
+    TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S") | "Script executed in: $G $TOTAL_TIME seconds $N" | tee -a $LOGS_FILE
+}
